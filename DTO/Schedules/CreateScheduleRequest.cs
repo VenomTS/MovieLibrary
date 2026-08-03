a09dd50bed431b8b049d132e080c7da656474448
LocalAdmin <the.waster.mail@gmail.com>
2026-08-03 14:15:28 +0200

    Remove Unneccessary DTOs

--- DTO/Schedules/CreateScheduleRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace DTO.Schedules;
-
-public class CreateScheduleRequest
-{
-    public DateOnly StartDate { get; set; }
-    public DateOnly? EndDate { get; set; }
-    public CreateRecurrenceRuleRequest RecurrenceRule { get; set; }
-}
